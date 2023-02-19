70e2bb402d2365d54dac764fab6f9931ddf5c2ed
samuelamei <samuelemekui@gmail.com>
2023-02-19 12:10:27 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include<stdio.h>
-
 /*
  *programming, building a multilingual
  *
  */
-
 int main(void)
 {
-
-     puts("\"Programming is like building a multilingual puzzle");
-
-     return 0;
-
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
